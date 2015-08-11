092ccd06cb15cbc954362f9d31a6e7e6b6797b84
vits <vits@localhost>
2015-08-10 19:13:17 -0500

    Control de Cambios Modificar Elementos

--- blocks/inventarios/gestionElementos/modificarElemento/funcion/modificar.php
+++ blocks/inventarios/gestionElementos/modificarElemento/funcion/modificar.php
@@ -26,9 +26,15 @@ class RegistradorOrden {
 	function procesarFormulario() {
 		$conexion = "inventarios";
 		$esteRecursoDB = $this->miConfigurador->fabricaConexiones->getRecursoDB ( $conexion );
+		var_dump ( $_REQUEST );
+		// --------------------- Cambio de tipo de Elemento
 		
+		$cadenaSql = $this->miSql->getCadenaSql ( 'Informacion_Elemento', $_REQUEST ['id_elemento'] );
+		
+		$info_elemento = $esteRecursoDB->ejecutarAcceso ( $cadenaSql, "busqueda" );
+		
+		$info_elemento = $info_elemento [0];
 		$_REQUEST ['total_iva_con'] = round ( $_REQUEST ['total_iva_con'] );
-		
 		
 		if ($_REQUEST ['id_tipo_bien'] == 1) {
 			
@@ -52,7 +58,7 @@ class RegistradorOrden {
 			
 			$cadenaSql = $this->miSql->getCadenaSql ( 'actualizar_elemento_tipo_1', $arreglo );
 			
-			$elemento = $esteRecursoDB->ejecutarAcceso ( $cadenaSql, "acceso" );
+			// $elemento = $esteRecursoDB->ejecutarAcceso ( $cadenaSql, "acceso" );
 		} else if ($_REQUEST ['id_tipo_bien'] == 2) {
 			
 			$arreglo = array (
@@ -76,7 +82,7 @@ class RegistradorOrden {
 			
 			$cadenaSql = $this->miSql->getCadenaSql ( 'actualizar_elemento_tipo_1', $arreglo );
 			
-			$elemento = $esteRecursoDB->ejecutarAcceso ( $cadenaSql, "acceso" );
+			// $elemento = $esteRecursoDB->ejecutarAcceso ( $cadenaSql, "acceso" );
 		} else if ($_REQUEST ['id_tipo_bien'] == 3) {
 			
 			if ($_REQUEST ['tipo_poliza'] == 0) {
@@ -125,24 +131,63 @@ class RegistradorOrden {
 			
 			$cadenaSql = $this->miSql->getCadenaSql ( 'actualizar_elemento_tipo_2', $arreglo );
 			
-			$elemento = $esteRecursoDB->ejecutarAcceso ( $cadenaSql, "acceso" );
+// 			$elemento = $esteRecursoDB->ejecutarAcceso ( $cadenaSql, "acceso" );
 		}
 		
-		
-		$cadenaSql = $this->miSql->getCadenaSql ( 'consultar_placa_actulizada', $_REQUEST ['id_elemento'] );
-		$placa = $esteRecursoDB->ejecutarAcceso ( $cadenaSql, "busqueda" );
+		if ($info_elemento ['tipo_bien'] != $_REQUEST ['id_tipo_bien']) {
+			
+			if (($info_elemento ['tipo_bien']==2 || $info_elemento ['tipo_bien']==3 )&&$_REQUEST ['id_tipo_bien'] == 1) {
+				$cadenaSql = $this->miSql->getCadenaSql ( 'consultar_elementos_individuales', $_REQUEST ['id_elemento'] );
+				$elementos_Individuales = $esteRecursoDB->ejecutarAcceso ( $cadenaSql, "busqueda" );
+				
+				foreach ( $elementos_Individuales as $valor ) {
+					
+					$cadenaSql = $this->miSql->getCadenaSql ( 'inhabilitar_elementos_individuales', $valor ['id_elemento_ind'] );
+					$elementos_Individuales = $esteRecursoDB->ejecutarAcceso ( $cadenaSql, "acceso" );
+				}
+				
+				$cadenaSql = $this->miSql->getCadenaSql ( 'idElementoMaxIndividual' );
+				$elemento_id_max_indiv = $esteRecursoDB->ejecutarAcceso ( $cadenaSql, "busqueda" );
+				
+				$elemento_id_max_indiv = $elemento_id_max_indiv [0] [0] + 1;
+				
+				for($i = 0; $i < $_REQUEST ['cantidad']; $i ++) {
+					$arregloElementosInv = array (
+							date ( 'Y-m-d' ),
+							NULL,
+							($_REQUEST ['serie'] != '') ? $_REQUEST ['serie'] : null,
+							$_REQUEST ['id_elemento'],
+							$elemento_id_max_indiv 
+					);
+					
+					$cadenaSql = $this->miSql->getCadenaSql ( 'ingresar_elemento_individual', $arregloElementosInv );
+					
+					$elemento_id [$i] = $esteRecursoDB->ejecutarAcceso ( $cadenaSql, "busqueda" );
+					
+					$elemento_id_max_indiv = $elemento_id [$i] [0] [0] + 1;
+				}
+			}else if ($info_elemento ['tipo_bien']==1 &&($_REQUEST ['id_tipo_bien'] == 2 ||$_REQUEST ['id_tipo_bien'] == 3)  ) {
+				echo "stiv  entro de nuevo";
+				
+				
+				
+				
+				
+			}
+		}
+		exit;
+		// -----------------
 		
 		if ($elemento) {
 			
 			redireccion::redireccionar ( 'inserto', $_REQUEST ['id_elemento'] );
-			exit();
+			exit ();
 		} else {
 			
 			redireccion::redireccionar ( 'noInserto' );
-			exit();
+			exit ();
 		}
 	}
-	
 }
 
 $miRegistrador = new RegistradorOrden ( $this->lenguaje, $this->sql, $this->funcion );
